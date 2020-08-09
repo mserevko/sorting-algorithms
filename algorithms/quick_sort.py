adff24f57686d7b6f9228de2dbeea14ffe3fb715
dsadasdas dsadsadas <mike13.lv@gmail.com>
2020-08-10 01:54:59 +0300

    added doc and rand int to quick sort

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,10 @@
+"""
+Quick Sort is one of the most efficient sorting algorithms.
+It is based on the splitting of the input list into smaller lists.
+Quick Sort works better with smaller data sets in comparison to merge sort.
+"""
+
+import random
+
+rand_list = [random.randint(1, 100) for i in range(0,8)]
+
