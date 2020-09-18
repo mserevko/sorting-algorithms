fd4e24af9d7c365d91a0e6765a0fb1e9778542ea
dsadasdas dsadsadas <mike13.lv@gmail.com>
2020-09-18 23:00:30 +0300

    added docstring to swap func

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -8,3 +8,16 @@ import random
 
 rand_list = [random.randint(1, 100) for i in range(0,8)]
 
+def swap(arr: list, i: int, k: int) -> None:
+    """
+
+    :param arr: input array
+    :type arr: list
+    :param i: first element to swap in list
+    :type i: int
+    :param k: second element to swap in list
+    :type k: int
+    :return: None
+    """
+    arr[i], arr[k] = arr[k], arr[i]
+
